b81ea1a7cbb1cf66cc73803da0d4e1692a596cf1
rishabgaddi <rishab03@live.com>
2022-10-25 15:31:37 +0200

    Added animated gradient text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fireworks } from '@fireworks-js/react';
+import styled from "styled-components";
+import { keyframes } from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Fireworks
+        options={{
+          rocketsPoint: {
+            min: 0,
+            max: 100
+          }
+        }}
+        style={{
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          position: 'fixed',
+          background: '#000'
+        }}
+        />
+        <div style={{
+          position: "absolute",
+          color: "white",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <AnimatedGradientText>Happy Birthday Señorita!</AnimatedGradientText>
+          <h1 style={{marginLeft: "5px"}}>🎂 ❤️</h1>
+        </div>
+    </>
   );
 }
+
+const gradient = keyframes`
+{
+0% {
+  background-position: 0 50%;
+}
+50% {
+  background-position: 100% 50%;
+}
+100% {
+  background-position: 0 50%;
+}}
+`;
+const AnimatedGradientText = styled.h1`
+  animation: ${gradient} 5s ease-in-out infinite;
+  background: linear-gradient(to right, #ee9ca7, #ffdde1, #2193b0, #6dd5ed);
+  background-size: 300%;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+`;
 
 export default App;
